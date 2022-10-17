d783c8a78a625e37d2d243f1f0fe25aaa168fe2e
Han Xheng Chew <81433191+shaaaaame@users.noreply.github.com>
2022-10-17 15:01:42 -0400

    Update Intro.jsx

--- src/components/Home/Intro/Intro.jsx
+++ src/components/Home/Intro/Intro.jsx
@@ -1,6 +1,7 @@
 import { useRef, React } from 'react';
 import { NavLink } from 'react-router-dom';
 import intro from '../../../assets/intro_tab.png';
+import resume from '../../../assets/HAN_XHENG-RESUME.pdf';
 import { useIsVisible } from 'react-is-visible';
 import classNames from 'classnames';
 
@@ -26,8 +27,8 @@ function Intro() {
             <h1 className='intro-title'>hey!</h1>
             <p className='intro-body'>i’m han xheng from malaysia, currently studying computer science and economics at the university of toronto, canada.</p>
             <div className='intro-links'>
-              <h3 className='intro-link'><b><NavLink to="/about">see more about me -{'>'}</NavLink></b></h3> {/* add href */}
-              <h3 className='intro-link'><b><a href="#">resume pdf -{'>'}</a></b></h3> {/* add href */}
+              <h3 className='intro-link'><b><NavLink to="/about">see more about me -{'>'}</NavLink></b></h3>
+              <h3 className='intro-link'><b><a href={resume}>resume pdf -{'>'}</a></b></h3>
             </div>
         </div>
     </div>
